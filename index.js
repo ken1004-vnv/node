9f54429913208ffe9f5a951539a8d40c81b30e44
Ngoc2004-tnvn <thainguyenvanngoc1042004@gmail.com>
2024-10-05 10:09:20 +0700

    bai tap buoi 4

--- index.js
+++ index.js
@@ -1,63 +1,8 @@
-const { log } = require('console');
-const { writeFile } = require('fs');
-const http = require('http');
-const os = require('os');
-// const Logger = require('./logger');
-// const logger = new Logger();
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-
-var information = {
-  OSType: os.type(),
-  Platform: os.platform(),
-  RAM: os.totalmem(),
-  USEDRAM: os.totalmem() - os.freemem(),
-  CPU: os.cpus()
-};
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(JSON.stringify(information, null, 2));
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-writeFile('D:\NodeJs\\index.txt', JSON.stringify(information, null, 2), (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  // logger.log('print computer\'s infos');
-  console.log('Completed task!');
-});
-
-
-// function getData(id) {
-
-//   let c ;
-//   setTimeout(() =>  {
-// c= {
-//    id,
-//       name: "ngoc"
-//  }
-//   },0)
-//     return c;
-// }
-
-// let a = getData(10);
-// console.log(a);
-
-
+//callback hell
 function getData(callback) {
   setTimeout(() => {
       console.log('Introduced');
-      callback('ngoc');
+      callback('Tran Viet Chinh');
   }, 1000);
 }
 function processData(data, callback) {
@@ -72,10 +17,23 @@ function saveData(processedData, callback) {
       callback();
   }, 1000);
 }
+
+
+
 getData(function(data) {
   processData(data, function(processedData) {
       saveData(processedData, function(saveData) {
         console.log('Chay xong');
       });
   });
+});
+
+
+async function abc(){
+  // await 
+  return "hello world";
+}
+abc().then((data) => {
+  console.log(data);
+
 });
